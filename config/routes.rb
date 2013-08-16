0ac169250037bcfc3739d4cfb893bcc80a6a0336
howarto <howarto@ymail.com>
2013-08-16 16:20:05 +0200

    Changes for deploy correctly on Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Tutorails::Application.routes.draw do
-  get "welcome/index"
+  #get "welcome/index"
+  match '/index', to: 'welcome#index', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
