2821c6534e43ee373922adb0bb38e148fbbef185
howarto <howarto@ymail.com>
2013-08-20 15:34:23 +0200

    Change user and pass

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  http_basic_authenticate_with name: "1234", password: "1234", except: [:index, :show]
   
 
   def update
