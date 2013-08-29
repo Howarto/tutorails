0466bd9af73275fae7dff243404f6cd1919b240e
howarto <howarto@ymail.com>
2013-08-29 20:25:30 +0200

    New tutorial about Bootstrap & Rails!

---
+++ Bootstrap/README.md
@@ -0,0 +1,6 @@
+Guía sacada de => http://railscasts.com/episodes/328-twitter-bootstrap-basics
+
+RESUMEN: Bootstrap, el famoso framework para la web pero aplicado para rails.
+
+Rails => 4.0.0
+Ruby => 2.0.0
---
+++ Bootstrap/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bootstrap::Application.config.secret_key_base = '5d02c0616af12b08b140134d233ccc8c8b7096e10004d4665ee5961cb16c0b9d19a81703aaa626125c9eb30d00b2547f5ea1b6035163067eb462687a84a845c9'
